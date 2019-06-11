4b2a704b0d08b3edbf19b7413337a78fa55ce447
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-11 20:41:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,6 +25,12 @@ def read_from_hash(hash, key)
  hash[key]
 end
 
-def update_counting_hash
+def update_counting_hash(hash,key)
+  if hash[key]
+    hash[key] += 1 
+  else 
+    hash[key] = 1 
+end 
+return hash
   
 end
